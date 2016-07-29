4457b0394935111d194bc0a8ca4791a2017b1d64
ORANGEAPPS-SOL <ORANGEAPPS-SOL>
2016-07-29 12:07:16 +0800

    sol commit

--- index.php
+++ index.php
@@ -269,41 +269,41 @@ switch (ENVIRONMENT)
 	define('APPPATH', $application_folder.DIRECTORY_SEPARATOR);
 
 	// The path to the "views" directory
-	if ( ! isset($view_folder[0]) && is_dir(APPPATH.'views'.DIRECTORY_SEPARATOR))
-	{
-		$view_folder = APPPATH.'views';
-	}
-	elseif (is_dir($view_folder))
-	{
-		if (($_temp = realpath($view_folder)) !== FALSE)
-		{
-			$view_folder = $_temp;
-		}
-		else
-		{
-			$view_folder = strtr(
-				rtrim($view_folder, '/\\'),
-				'/\\',
-				DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
-			);
-		}
-	}
-	elseif (is_dir(APPPATH.$view_folder.DIRECTORY_SEPARATOR))
-	{
-		$view_folder = APPPATH.strtr(
-			trim($view_folder, '/\\'),
-			'/\\',
-			DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
-		);
-	}
-	else
-	{
-		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-		echo 'Your view folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF;
-		exit(3); // EXIT_CONFIG
-	}
-
-	define('VIEWPATH', $view_folder.DIRECTORY_SEPARATOR);
+	// if ( ! isset($view_folder[0]) && is_dir(APPPATH.'views'.DIRECTORY_SEPARATOR))
+	// {
+	// 	$view_folder = APPPATH.'views';
+	// }
+	// elseif (is_dir($view_folder))
+	// {
+	// 	if (($_temp = realpath($view_folder)) !== FALSE)
+	// 	{
+	// 		$view_folder = $_temp;
+	// 	}
+	// 	else
+	// 	{
+	// 		$view_folder = strtr(
+	// 			rtrim($view_folder, '/\\'),
+	// 			'/\\',
+	// 			DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
+	// 		);
+	// 	}
+	// }
+	// elseif (is_dir(APPPATH.$view_folder.DIRECTORY_SEPARATOR))
+	// {
+	// 	$view_folder = APPPATH.strtr(
+	// 		trim($view_folder, '/\\'),
+	// 		'/\\',
+	// 		DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
+	// 	);
+	// }
+	// else
+	// {
+	// 	header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
+	// 	echo 'Your view folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF;
+	// 	exit(3); // EXIT_CONFIG
+	// }
+	//
+	// define('VIEWPATH', $view_folder.DIRECTORY_SEPARATOR);
 
 /*
  * --------------------------------------------------------------------
@@ -312,4 +312,4 @@ switch (ENVIRONMENT)
  *
  * And away we go...
  */
-require_once BASEPATH.'core/CodeIgniter.php';
+// require_once BASEPATH.'core/CodeIgniter.php';
